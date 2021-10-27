124735200dfd87e4eb44a87010e81236ee0b30bc
spiczi <spiczmuller.richard@gmail.com>
2021-10-27 14:27:00 +0200

    Navigation bar, styled components error

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-import SearchBar from './Search';
+import React from "react";
+import './App.css';
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './pages';
+import About from './pages/about';
+import Contact from './pages/contact';
+import SignUp from './pages/signup';
+import SignIn from './pages/signin';
 
 function App() {
-  return (
-    <><h1>HELLO</h1><SearchBar /></>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/signin" component={SignIn} />
+                <Route path="/sign-up" component={SignUp} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
